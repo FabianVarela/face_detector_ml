3b96ea85a070030d2fb19627a39e304c1b05ee63
Fabian Varela Bonett <fvarela@smarttaxi.co>
2019-03-19 17:25:31 -0500

    Implement the painter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:face_detector_ml/painter.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
@@ -28,11 +30,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   File _imageFile;
+  Size _imageSize;
   List<Face> _faces;
 
   void _pickImage() async {
     setState(() {
       _imageFile = null;
+      _imageSize = null;
     });
 
     final imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
@@ -40,15 +44,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
     if (imageFile != null) {
       final image = FirebaseVisionImage.fromFile(imageFile);
-      final faceDetector = FirebaseVision.instance.faceDetector();
+      final faceDetector = FirebaseVision.instance.faceDetector(
+          FaceDetectorOptions(
+              mode: FaceDetectorMode.accurate, enableLandmarks: true));
       faces = await faceDetector.processImage(image);
     }
 
     setState(() {
       if (mounted) {
+        _getImageSize(imageFile);
+
         _imageFile = imageFile;
         _faces = faces;
       }
+    });
+  }
+
+  Future<void> _getImageSize(File imageFile) async {
+    final Completer<Size> completer = Completer<Size>();
+
+    final Image image = Image.file(imageFile);
+    image.image.resolve(const ImageConfiguration()).addListener(
+      (ImageInfo info, bool _) {
+        completer.complete(Size(
+          info.image.width.toDouble(),
+          info.image.height.toDouble(),
+        ));
+      },
+    );
+
+    final Size imageSize = await completer.future;
+
+    setState(() {
+      _imageSize = imageSize;
     });
   }
 
@@ -62,6 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ? Center(child: Text('No image selected.'))
           : ImageAndFaces(
               imageFile: _imageFile,
+              imageSize: _imageSize,
               faces: _faces,
             ),
       floatingActionButton: FloatingActionButton(
@@ -74,10 +103,19 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class ImageAndFaces extends StatelessWidget {
-  ImageAndFaces({this.imageFile, this.faces});
+  ImageAndFaces({this.imageFile, this.imageSize, this.faces});
 
   final File imageFile;
+  final Size imageSize;
   final List<Face> faces;
+
+  CustomPaint _buildResults(Size imageSize, List<Face> faces) {
+    CustomPainter painter = FaceDetectorPainter(imageSize, faces);
+
+    return CustomPaint(
+      painter: painter,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,6 +137,20 @@ class ImageAndFaces extends StatelessWidget {
             children: faces.map<Widget>((f) => FaceCoordinates(f)).toList(),
           ),
         ),
+        Flexible(
+          flex: 1,
+          child: imageSize == null || faces == null
+              ? const Center(
+                  child: Text(
+                    'Scanning...',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                )
+              : _buildResults(imageSize, faces),
+        )
       ],
     );
   }
@@ -111,9 +163,20 @@ class FaceCoordinates extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final FaceLandmark leftEar = face.getLandmark(FaceLandmarkType.leftEar);
+
+    print("Head Y: ${face.headEulerAngleY}");
+    print("Head Z: ${face.headEulerAngleZ}");
+
+    if (leftEar != null) print("LeftEar: ${leftEar.position}");
+
+    print("Probability: ${face.smilingProbability}");
+    print("Tracking: ${face.trackingId}");
+
     final pos = face.boundingBox;
     return ListTile(
       title: Text("(${pos.top}, ${pos.left}), (${pos.bottom}, ${pos.right})"),
+      subtitle: Text("Probabilidad de felicidad: ${face.smilingProbability}"),
     );
   }
 }
---
+++ lib/painter.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+
+class FaceDetectorPainter extends CustomPainter {
+  FaceDetectorPainter(this.absoluteImageSize, this.faces);
+
+  final Size absoluteImageSize;
+  final List<Face> faces;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0
+      ..color = Colors.red;
+
+    for (Face face in faces) {
+      canvas.drawRect(
+        Rect.fromLTRB(
+          face.boundingBox.left * scaleX,
+          face.boundingBox.top * scaleY,
+          face.boundingBox.right * scaleX,
+          face.boundingBox.bottom * scaleY,
+        ),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.faces != faces;
+  }
+}
